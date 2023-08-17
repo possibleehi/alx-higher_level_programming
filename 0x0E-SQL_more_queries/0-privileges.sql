bdf2430338d00dfe487d1bf9b32a8402f34e5042
possibleehi <possibleehi@gmail.com>
2023-08-17 05:27:49 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- show privilege from user 1 and user 2
+-- LIsts all privileges of the MySQL Users `user_0d_1`
+-- and `user_0d_2` in `localhost`.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
