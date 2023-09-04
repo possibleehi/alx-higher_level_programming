06356dbd5cf16baac81ffe25511a7cb852d54399
possibleehi <123018117+possibleehi@users.noreply.github.com>
2023-09-04 18:48:11 +0100

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/0-list_databases.sql
+++
@@ -1,2 +0,0 @@
--- lists all the databases in my MYSQL server
-SHOW DATABASES;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++
@@ -1,4 +0,0 @@
--- lists all the tables of a DB in my MySQL server
--- DB name will be paased as an arg of mysql command
-
-SHOW TABLES;
